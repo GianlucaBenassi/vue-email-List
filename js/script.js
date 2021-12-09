90de6536120a2d0778ef54622094abb4cc7c8ce3
Gianluca Benassi <benox1199@gmail.com>
2021-12-09 13:59:12 +0100

    create random emails array

--- js/script.js
+++ js/script.js
@@ -2,11 +2,30 @@ const app = new Vue({
     el: '#root',
 
     data: {
-
+        emails: []
     },
 
     methods: {
 
+    },
+
+    created: function() {
+
+        for (let i = 0; i < 10; i++) {
+
+            // make request for a random email
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                // handle success
+                this.emails.push(response.data.response);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            });
+
+        }
+
     }
-    
+
 });
